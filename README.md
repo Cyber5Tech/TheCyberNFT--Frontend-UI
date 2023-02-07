6befd4530f69be44d116f2a894ef4a6b81601e08
Cyber5Tech <dhayoralabi@gmail.com>
2023-02-07 13:13:43 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-# buildspace Mint Your Own NFT Collection
+# This project is from the buildspace course "Mint Your Own NFT Collection"
 
-### **Welcome 👋**
-To get started with this course, clone this repo and follow these commands:
+## This is the frontend app of the project built with React
 
-1. Run `npm install` at the root of your directory
-2. Run `npm run start` to start the project
-3. Start coding!
-
-### **Questions?**
-Have some questions make sure you head over to your [buildspace Dashboard](https://app.buildspace.so/courses/CO961ddb5f-f428-4608-9949-a9a2f461eb3f) and link your Discord account so you can get access to helpful channels and your instructor!
+### How to run
+1. Clone this repo or download
+1. Run `npm install` at the root of this project directory
+3. Run `npm run start` fire up the project
--- src/App.js
+++ src/App.js
@@ -1,30 +1,154 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
+import TheCyberNFT from './utils/TheCyberNFT.json';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'dhayoralabi';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
-const TOTAL_MINT_COUNT = 50;
+const OPENSEA_LINK = 'https://testnets.opensea.io/collection/animelover-v3';
+const BUILDSPACE_HANDLE = '_buildspace';
+const BUILDSPACE_TWIITER_LINK = `https://twitter.com/${BUILDSPACE_HANDLE}`;
+//const TOTAL_MINT_COUNT = 50;
+
+const CONTRACT_ADDRESS = "0x94cF58FD7e68aAe9DA98E1138592E86Ce7D8CC5F";
 
 const App = () => {
-  // Render Methods
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Install Metamask");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({method: 'eth_accounts'});
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account", account);
+                                    setCurrentAccount(account)
+      
+      //event listener to check if a user has previously connected and authorized their wallet
+      setupEventListener()
+    } else {
+      console.log("No authorized accounts found")
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Install Metamask");
+        return;
+      }
+
+      const accounts = await ethereum.request({method: "eth_requestAccounts"});
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+      //Setup listner to check if a  user has previously connected and authorized a wallet
+      setupEventListener()
+    } catch (error) {
+      console.log (error)
+    }
+  }
+
+  //Setup listener
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, TheCyberNFT.abi, signer);
+
+        //captures events from contract (like webhooks)
+        connectedContract.on("NewCyberNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          // eslint-disable-next-line no-template-curly-in-string
+          alert(`Your NFT has been minted and sent to your wallet, It might take up to 10 mins to appear on Opensea. Here the link to your NFT on Opensea https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener")
+
+    } else {
+      console.log("Ethereum object does not exist");
+    }
+  } catch (error) {
+    console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, TheCyberNFT.abi, signer);
+
+        console.log("Popping up wallet to pay gas")
+        let nftTxn = await connectedContract.makeACyberNFT();
+
+        console.log("Mining, Please wait.");
+        await nftTxn.wait();
+        console.log(nftTxn);
+        console.log("Minted, see transaction: https://goerli.etherscan.io/tx");
+      } else {
+        console.log("Ethereum object does not exist");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
-      Connect to Wallet
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      Connect Wallet
     </button>
   );
+
+  const renderMintUI = () => (
+    <button onClick={askContractToMintNft} className="cta-button mint-button">
+      Mint NFT
+    </button>
+  )
+  const opensea = () => (
+    <a
+      href={OPENSEA_LINK}
+      target= "_blank"
+      rel="noreferrer"
+      >
+        <button className='cta-button opensea-button'>Buy on Opensea</button>
+    </a>
+  )
 
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
+          <p className="header gradient-text">Anime Lover</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+            Each unique, Each beautiful, Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
+          {opensea()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -33,7 +157,13 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}
+          <a className="footer-text"
+            href={BUILDSPACE_TWIITER_LINK}
+            target="_blank"
+            rel="noreferrer"> {`on @${BUILDSPACE_HANDLE}`}
+          </a>
+          </a>
         </div>
       </div>
     </div>
